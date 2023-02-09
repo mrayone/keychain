70586eb814c61dbd0b976d09a43d2c085ffa7504
Maycon <maycon.rayone@gmail.com>
2023-02-08 21:58:11 -0300

    chore(*): remove unused code

--- product/domain.go
+++
@@ -1,7 +0,0 @@
-package product
-
-type Product struct {
-	ID    int     `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-}
--- scripts/init-git.sh
+++ scripts/init-git.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+go install -v github.com/go-critic/go-critic/cmd/gocritic@latest
+
 cp -i ./scripts/pre-commit.sh ./.git/hooks/pre-commit
 cp -i ./scripts/pre-push.sh ./.git/hooks/pre-push
 
